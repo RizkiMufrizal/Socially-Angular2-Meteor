f0b1a91375f313f78f12b0bc218ebf6ef98f3545
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-04 23:08:50 +0700

    update bootstrap

--- client/app.ts
+++ client/app.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
 import { Component } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
+import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { MeteorComponent } from 'angular2-meteor';
 
 @Component({
   selector: 'app',
   templateUrl: '/client/app.html'
 })
-class Socially { }
+class Socially extends MeteorComponent { }
 
 bootstrap(Socially);
