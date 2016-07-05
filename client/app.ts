10f4e894aec7852271bd01eb747ccf2272b55f2a
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-05 10:54:04 +0700

    update router

--- client/app.ts
+++ client/app.ts
@@ -1,12 +1,20 @@
 import 'reflect-metadata';
-import { Component } from '@angular/core';
+import { Component, provide } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { MeteorComponent } from 'angular2-meteor';
+import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig } from '@angular/router-deprecated';
+import { APP_BASE_HREF } from '@angular/common';
+
+import { HomeComponent } from './imports/home/home-component';
 
 @Component({
   selector: 'app',
-  templateUrl: '/client/app.html'
+  templateUrl: '/client/app.html',
+  directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+  { path: '/', name: 'Home', component: HomeComponent },
+])
 class Socially extends MeteorComponent { }
 
-bootstrap(Socially);
+bootstrap(Socially, [ROUTER_PROVIDERS, provide(APP_BASE_HREF, { useValue: '/' })]);
