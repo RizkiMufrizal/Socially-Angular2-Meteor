cfdd995f6ec66819edfcd3d51f7111cda6a33670
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-05 10:53:26 +0700

    tambah contoh home

---
+++ client/imports/home/home-component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-component',
+  templateUrl: '/client/imports/home/home-template.html'
+})
+export class HomeComponent { }
