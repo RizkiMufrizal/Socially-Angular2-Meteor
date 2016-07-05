1c7c09c681e8d08f000b28d3a46dee312a78d77f
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-05 16:37:19 +0700

    tambah MdIconRegistry

--- client/imports/post/post-component.ts
+++ client/imports/post/post-component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 import { Posts } from '../../../collections/posts/posts';
 
 @Component({
   selector: 'post-component',
-  templateUrl: '/client/imports/post/post-template.html'
+  templateUrl: '/client/imports/post/post-template.html',
+  directives: [MdIcon],
+  providers: [MdIconRegistry]
 })
 export class PostComponent implements OnInit {
 
