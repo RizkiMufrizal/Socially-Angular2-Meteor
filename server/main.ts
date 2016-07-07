7b5c4451339283a1802c64d2d045a5cc40f49325
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-07 15:26:53 +0700

    hapus post dan user pada saat startup

--- server/main.ts
+++ server/main.ts
@@ -4,45 +4,6 @@ import { Meteor } from 'meteor/meteor';
 import { Posts } from '../collections/posts/posts';
 
 Meteor.startup(() => {
-  if (Posts.find().count() === 0) {
-    let posts = [
-      {
-        username: 'rizki',
-        status: 'latihan angular2 meteor',
-        dateTime: new Date(),
-        comments: [
-          {
-            username: 'mufrizal',
-            comment: 'sedap'
-          }
-        ],
-        likes: [
-          {
-            username: 'mufrizal'
-          }
-        ]
-      },
-      {
-        username: 'mufrizal',
-        status: 'angular 2 meteor',
-        dateTime: new Date(),
-        comments: [
-          {
-            username: 'rizki',
-            comment: 'oke'
-          }
-        ],
-        likes: [
-          {
-            username: 'rizki'
-          }
-        ]
-      }
-    ];
-
-    for (let i = 0; i < posts.length; i++) {
-      Posts.insert(posts[i]);
-    }
-
-  }
+  Posts.remove({});
+  Meteor.users.remove({});
 });
