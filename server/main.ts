a24eea163cae529ebfdd540e5f7de871b457a9a7
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-08 16:30:33 +0700

    update main server

--- server/main.ts
+++ server/main.ts
@@ -2,8 +2,9 @@ import './posts';
 import '../collections/posts/methods';
 import { Meteor } from 'meteor/meteor';
 import { Posts } from '../collections/posts/posts';
+import { TimeLines } from '../collections/timelines/timelines';
 
 Meteor.startup(() => {
   Posts.remove({});
-  Meteor.users.remove({});
+  TimeLines.remove({});
 });
